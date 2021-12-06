993fbf0a6f5e9a7c486904e28f7b68c60fc3aa26
Dimitri Frizler <DiegosBogen@hotmail.de>
2021-12-06 19:04:33 +0100

    Assignment 3.1 and 3.2

--- rtgi-2021-a03/libgi/material.cpp
+++ rtgi-2021-a03/libgi/material.cpp
@@ -1,6 +1,7 @@
 #include "material.h"
 #include "scene.h"
 #include "util.h"
+#include <iostream>
 
 using namespace glm;
 
@@ -16,8 +17,14 @@ vec3 layered_brdf::f(const diff_geom &geom, const vec3 &w_o, const vec3 &w_i) {
 
 vec3 lambertian_reflection::f(const diff_geom &geom, const vec3 &w_o, const vec3 &w_i) {
 	if (!same_hemisphere(w_i, geom.ns)) return vec3(0);
-	// todo
-	return vec3(0);
+
+	//todo
+	//return vec3(0);
+
+	vec3 p=geom.albedo();
+	return p/pi;
+
+	
 }
 
 
@@ -26,6 +33,23 @@ vec3 lambertian_reflection::f(const diff_geom &geom, const vec3 &w_o, const vec3
 vec3 phong_specular_reflection::f(const diff_geom &geom, const vec3 &w_o, const vec3 &w_i) {
 	if (!same_hemisphere(w_i, geom.ng)) return vec3(0);
 	// todo
+
+	vec3 n=geom.ng; // in Lösung ns und nicht ng ?
+	vec3 w_r=2.0f*n*dot(w_i,n)-w_i;
+
+	float n_s=exponent_from_roughness(geom.mat->roughness);
+	float dot_wr_w_o=cdot(w_r,w_o);
+	float norm=(n_s+1.0f)*one_over_2pi;
+	float f_phong=norm*(powf(dot_wr_w_o,n_s));
+
+
+	//Solution : 
+	//float exponent = exponent_from_roughness(geom.mat->roughness);
+	//vec3 r = 2.0f * geom.ns * dot(geom.ns, w_i) - w_i;
+	//float cos_theta = cdot(w_o, r);
+	//const float norm_f = (exponent + 2.0f) * one_over_2pi;
+	//return (coat ? vec3(1) : geom.albedo()) * powf(cos_theta, exponent) * norm_f * cdot(w_i,geom.ns);
+
 	return vec3(0);
 }
 
