83c367e0ed1a7f1e51e2ec410b77bfc5c79db987
Dimitri Frizler <DiegosBogen@hotmail.de>
2021-12-07 18:15:15 +0100

    end 3

--- rtgi-2021-a03/libgi/material.cpp
+++ rtgi-2021-a03/libgi/material.cpp
@@ -41,25 +41,14 @@ vec3 lambertian_reflection::f(const diff_geom &geom, const vec3 &w_o, const vec3
 
 vec3 phong_specular_reflection::f(const diff_geom &geom, const vec3 &w_o, const vec3 &w_i) {
 	if (!same_hemisphere(w_i, geom.ng)) return vec3(0);
-	// todo
 
-	//vec3 n=geom.ng; // in Lösung ns und nicht ng ?
-	//vec3 w_r=2.0f*n*dot(w_i,n)-w_i;
-	//float n_s=exponent_from_roughness(geom.mat->roughness);
-	//float dot_wr_w_o=cdot(w_r,w_o);
-	//float norm=(n_s+1.0f)*one_over_2pi;
-	//float f_phong=norm*(powf(dot_wr_w_o,n_s));
-
-
-	//Solution : 
-	//std::cout <<"werde benutzt"<<std::endl;
 	float exponent = exponent_from_roughness(geom.mat->roughness);
 	vec3 r = 2.0f * geom.ns * dot(geom.ns, w_i) - w_i;
 	float cos_theta = cdot(w_o, r);
 	const float norm_f = (exponent + 2.0f) * one_over_2pi;
 	return (coat ? vec3(1) : geom.albedo()) * powf(cos_theta, exponent) * norm_f * cdot(w_i,geom.ns); //modified phong brdf ? 
 
-	//return vec3(0);
+
 }
 
 
@@ -68,21 +57,66 @@ vec3 phong_specular_reflection::f(const diff_geom &geom, const vec3 &w_o, const 
 // In the following, make sure that corner cases are taken care of (positive
 // dot products, >0 denominators, correctly aligned directions, etc)
 
+
+#define sqr(x) ((x)*(x))
+
+inline float A(float theta, float m)
+{
+	float A=(1/2) *(sqrt(1+pow(m,2) *pow(tan(theta), 2 ) -1));  
+	return A;
+}
+
+
 inline float ggx_d(const float NdotH, float roughness) {
-	// todo TR NDF
-	return 0.0f;
+
+	float m = roughness;
+	float cos_theta_h=NdotH;
+
+	return ((m*m)/pi) * pow((1+(m*m-1) * cos_theta_h*cos_theta_h ),-2);
 }
 
 inline float ggx_g1(const float NdotV, float roughness) {
-	// todo TR Smith G_1
-	return 0.0f;
+
+	float omega=NdotV;
+
+	if(omega<0)
+	{
+		return 0.0f;
+	}
+	float m =roughness;
+
+	return 1/(1+A(omega,m));
 }
 
 
-
 vec3 gtr2_reflection::f(const diff_geom &geom, const vec3 &w_o, const vec3 &w_i) {
-	// todo full TR Microfacet BRDF, use ggx_d and ggx_g1
-	return vec3(0);
+
+
+	if (!same_hemisphere(geom.ng, w_i)) return vec3(0);
+
+	vec3 N = geom.ng;
+	vec3 L=w_i;
+	vec3 V=w_o;
+	vec3 H = normalize(V+L);
+
+	float HdotL =cdot(H,L);
+
+
+	// a dot b = |a| * |b| *  cos(phi)  ->  |a| =1 und |b|=1
+
+	float cos_theta_o=cdot(N, V);
+	float cos_theta_i=cdot(N, L);
+	float cos_theta_h=cdot(N,H);
+
+	const float roughness = geom.mat->roughness;
+
+	const float G = ggx_g1(cos_theta_o, roughness) * ggx_g1(cos_theta_i, roughness);
+	const float F = fresnel_dielectric(HdotL, 1.f, geom.mat->ior);
+	const float D = ggx_d(cos_theta_h, roughness);
+	const float microfacet = (F * D * G) / (4 * abs(cos_theta_o) * abs(cos_theta_i));
+
+    return coat ? vec3(microfacet) : geom.albedo() * microfacet;
+
 }
 
 
@@ -103,7 +137,12 @@ brdf *new_brdf(const std::string name, scene &scene) {
 		else if (name == "gtr2")
 			f = new gtr2_reflection;
 		else if (name == "layered-gtr2") {
-			throw std::runtime_error(std::string("Not implemented yet: ") + name);
+
+			brdf *base = new_brdf("lambert", scene);
+			specular_brdf *coat = dynamic_cast<specular_brdf*>(new_brdf("gtr2", scene));
+			assert(coat);
+			f = new layered_brdf(coat, base);
+			//throw std::runtime_error(std::string("Not implemented yet: ") + name);
 		}
 		else
 			throw std::runtime_error(std::string("No such brdf defined: ") + name);
