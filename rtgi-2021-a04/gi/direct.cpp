40e169b12bd0f3a615c18ae3140b157d2be6f484
Dimitri Frizler <DiegosBogen@hotmail.de>
2021-12-14 13:30:38 +0100

    4.1 done

--- rtgi-2021-a04/gi/direct.cpp
+++ rtgi-2021-a04/gi/direct.cpp
@@ -23,6 +23,20 @@ gi_algorithm::sample_result direct_light::sample_pixel(uint32_t x, uint32_t y, u
 			flip_normals_to_ray(dg, view_ray);
 
 			// todo: compute direct lighting contribution
+			if (dg.mat->emissive != vec3(0))
+			{
+				radiance = dg.mat->emissive;
+			}
+			else
+			{
+				brdf *brdf = dg.mat->brdf;
+				// auto col = dg.mat->albedo_tex ? dg.mat->albedo_tex->sample(dg.tc) : dg.mat->albedo;
+				if (sampling_mode == sample_uniform)
+					radiance = sample_uniformly(dg, view_ray);
+				//else if (sampling_mode == sample_light)
+				//	radiance = sample_lights(dg, view_ray);
+			}
+
 		}
 		result.push_back({radiance,vec2(0)});
 	}
@@ -33,7 +47,29 @@ vec3 direct_light::sample_uniformly(const diff_geom &hit, const ray &view_ray) {
 	// set up a ray in the hemisphere that is uniformly distributed
 	vec2 xi = rc->rng.uniform_float2();
 	// todo: implement uniform hemisphere sampling
-	return vec3(0);
+
+	float z=xi.x;  //z =cos(theta)
+	float phi = 2*pi*xi.y;
+	float theta = acos(z); 
+
+	float sin_theta=sqrt(1.0f-z*z);
+	float cos_phi=cos(phi);
+	float sin_phi=sin(phi);
+
+	vec3 w_i(sin_theta *cos_phi, sin_theta*sin_phi, z);
+	w_i=align(w_i,hit.ng);
+	ray sample_ray(hit.x, w_i);
+
+	vec3 brightness(0);
+	triangle_intersection closest = rc->scene.single_rt->closest_hit(sample_ray);
+	if (closest.valid()) {
+		diff_geom dg(closest, rc->scene);
+		brightness = dg.mat->emissive;
+	}
+
+	float cos_theta_i=cdot(w_i, hit.ns);		
+	return 2*pi*brightness * hit.mat->brdf->f(hit,-view_ray.d, sample_ray.d)*cos_theta_i;
+
 }
 
 
